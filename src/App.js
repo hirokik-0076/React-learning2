1c5503eaecfe1910616f5ceb9b77d9649eee2750
Hiroki Kinoshita <hiroki.kinoshita@sun-asterisk.com>
2019-05-13 09:41:40 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // stateを定義してください
+    this.state={count:0}
+    
+  }
+  handleClick(){
+    this.setState({count:this.state.count+1})
+  }
+  render() {
+    return (
+      <div>
+        <h1>
+          {/* stateの値を表示してください */}
+          {this.state.count}
+      
+        </h1>
+        {/* <button>タグを追加してください */}
+        <button onClick={()=>{this.handleClick()}}>+</button>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
